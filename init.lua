65d1919f659d01eeaf253a6a6198eb7fc6d68d2d
treuliaux <tanguy.reuliaux@gmail.com>
2019-08-31 19:47:48 +0200

    Attached first panel to TradeSkillFrame

--- init.lua
+++ init.lua
@@ -2,11 +2,16 @@ local mainFrame = CreateFrame("Frame",nil,UIParent)
 mainFrame:Hide()
 mainFrame:SetFrameStrata("BACKGROUND")
 mainFrame:SetWidth(300)
-mainFrame:SetHeight(120)
-mainFrame:SetPoint("CENTER", 0, 0)
+mainFrame:SetHeight(400)
+
+--mainFrame:SetBackdrop({
+--	bgFile = "Interface\\ChatFrame\\ChatFrameBackground",
+--	insets = {top = 0, left = 0, bottom = 0, right = 0},
+--})
+--mainFrame:SetBackdropColor(1, 1, 1, 0)
 
 local t = mainFrame:CreateTexture(nil,"BACKGROUND")
-t:SetTexture("Interface\\Glues\\CharacterCreate\\UI-CharacterCreate-Factions.blp")
+t:SetTexture("Interface\\ChatFrame\\ChatFrameBackground")
 t:SetAllPoints(mainFrame)
 mainFrame.texture = t
 
@@ -16,10 +21,11 @@ mainFrame:SetScript(
     "OnEvent",
     function(self, event, ...)
 	if event == "TRADE_SKILL_SHOW" then
+		mainFrame:SetPoint("TOPLEFT", "TradeSkillFrame", "TOPRIGHT", -30, -15)
 		-- print(GetTradeSkillSelectionIndex())
 		local tradeskillName, currentLevel, maxLevel = GetTradeSkillLine();
-		print(GetFirstTradeSkill())
-		print(currentLevel)
+		-- print(GetFirstTradeSkill())
+		print(tradeskillName, currentLevel, maxLevel)
 		mainFrame:Show()
 	elseif event == "TRADE_SKILL_CLOSE" then
 		mainFrame:Hide()
