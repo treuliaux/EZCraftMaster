b1dace8cf2806645083dcbbfd78d05d47074dbce
Moorgrimm <plugmaster91@gmail.com>
2019-09-06 19:03:40 +0200

    Add text

--- init.lua
+++ init.lua
@@ -15,6 +15,11 @@ t:SetTexture("Interface\\ChatFrame\\ChatFrameBackground")
 t:SetAllPoints(mainFrame)
 mainFrame.texture = t
 
+mainFrame.text = mainFrame:CreateFontString(nil, "ARTWORK")
+mainFrame.text:SetFont("Fonts\\ARIALN.ttf", 15, "THICKOUTLINE, MONOCHROME")
+mainFrame.text:SetPoint("TOPLEFT",5,-5)
+mainFrame.text:SetTextColor(200, 100, 0)
+
 mainFrame:RegisterEvent("TRADE_SKILL_SHOW")
 mainFrame:RegisterEvent("TRADE_SKILL_CLOSE")
 mainFrame:SetScript(
@@ -23,13 +28,18 @@ mainFrame:SetScript(
 	if event == "TRADE_SKILL_SHOW" then
 		mainFrame:SetPoint("TOPLEFT", "TradeSkillFrame", "TOPRIGHT", -30, -15)
 		-- print(GetTradeSkillSelectionIndex())
-		local tradeskillName, currentLevel, maxLevel = GetTradeSkillLine();
 		-- print(GetFirstTradeSkill())
-		print(tradeskillName, currentLevel, maxLevel)
+		addSkillText(mainFrame)
+		-- print(tradeskillName, currentLevel, maxLevel)
 		mainFrame:Show()
 	elseif event == "TRADE_SKILL_CLOSE" then
 		mainFrame:Hide()
 	end
     end
 )
+
+function addSkillText(mainFrame)
+	local tradeskillName, currentLevel, maxLevel = GetTradeSkillLine();
+	mainFrame.text:SetText(tradeskillName .. " " .. currentLevel .. "/" .. maxLevel)
+end
 
